5f9f3761790b2e8572d0ce2e500a93e7183f9540
Jon Škoberne <jonchisko@gmail.com>
2025-04-30 18:51:22 +0200

    Solution 1

--- src/main.rs
+++ src/main.rs
@@ -159,7 +159,7 @@ fn main() {
 
         void main() {
             //gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
-            gl_Position = vec4(pos, 1.0);
+            gl_Position = vec4(pos.x, -pos.y, pos.z, 1.0);
             vertexColor = vec4(color, 1.0);
         }
     "#;
